248dc0439541ff2b3f7bc31b870dc65948509ca0
KJ-Hyeon <104378938+KJ-Hyeon@users.noreply.github.com>
2023-11-27 17:35:57 +0900

    [level unrated] Title: 각도기, Time: 0.01 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(angle: Int): Int {
+        var answer: Int = 0
+        answer = when(angle) {
+            in 1..89 -> 1
+            90 -> 2
+            in 91..179 -> 3
+            180 -> 4
+            else -> 0
+        }
+        return answer
+    }
+}
