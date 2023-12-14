0d4830a40caae64b4be2c4ef712ab2a0314332fa
KJ-Hyeon <104378938+KJ-Hyeon@users.noreply.github.com>
2023-12-14 20:07:12 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 10.70 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(strlist: Array<String>): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in strlist) {
+            answer += i.length
+        }
+        return answer
+    }
+}
