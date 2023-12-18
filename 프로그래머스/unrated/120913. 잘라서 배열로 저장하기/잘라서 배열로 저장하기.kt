80a5bad465309655cae2a7d49eb72565d8250504
KJ-Hyeon <104378938+KJ-Hyeon@users.noreply.github.com>
2023-12-18 15:08:36 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 21.04 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(my_str: String, n: Int): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        var endPoint = 0 
+        for(i in my_str.indices step n) {
+            if(my_str.length >=  i + n) answer += my_str.substring(i until n+i)
+            else answer += my_str.substring(i)
+        }
+        return answer
+    }
+}
+// chunked : 개수를 기준으로 문자열을 자르는 함수. 리턴값도 List
+// my_str.chunked(n).toTypedArray()
