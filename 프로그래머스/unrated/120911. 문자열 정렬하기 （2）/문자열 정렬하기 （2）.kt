bb35d896b9e3ea3176ed9e1ce69a0937f8c15aed
KJ-Hyeon <104378938+KJ-Hyeon@users.noreply.github.com>
2023-12-18 15:41:38 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 28.28 ms, Memory: 64.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = ""
+        answer = my_string.toLowerCase().toCharArray().sorted().joinToString("")
+        return answer
+    }
+}
